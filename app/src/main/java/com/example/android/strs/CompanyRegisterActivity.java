06e9901c61ea71f9f06ab1a870b3af5d46bc057a
Utkarsh Bajpai <utkarsh.bajpai2015@vit.ac.in>
2017-04-11 22:34:44 +0530

    Completed UI + Added Spinners

--- app/src/main/java/com/example/android/strs/CompanyRegisterActivity.java
+++ app/src/main/java/com/example/android/strs/CompanyRegisterActivity.java
@@ -2,22 +2,80 @@ package com.example.android.strs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
 public class CompanyRegisterActivity extends AppCompatActivity {
 
+        private EditText etCMode;
+        private EditText etCName;
+        private EditText etCUsername;
+        private EditText etCPassword;
+        private Button bCRegister;
+        private Spinner modeSpinner;
+
+    private int mode = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company_register);
 
-        final EditText etCMode = (EditText) findViewById(R.id.etCMode);
-        final EditText etCName = (EditText) findViewById(R.id.etCName);
-        final EditText etCUsername = (EditText) findViewById(R.id.etCUsername);
-        final EditText etCPassword = (EditText) findViewById(R.id.etCPassword);
-        final Button bCRegister = (Button) findViewById(R.id.bCRegister);
+        // Find all relevant views that we will need to read user input from
+        etCMode = (EditText) findViewById(R.id.etCMode);
+        etCName = (EditText) findViewById(R.id.etCName);
+        etCUsername = (EditText) findViewById(R.id.etCUsername);
+        etCPassword = (EditText) findViewById(R.id.etCPassword);
+        bCRegister = (Button) findViewById(R.id.bCRegister);
+        modeSpinner = (Spinner) findViewById(R.id.spinner_Cmode);
 
+        setupSpinner();
+
+    }
+
+    /**
+     * Setup the dropdown spinner that allows the user to select the gender of the pet.
+     */
+    private void setupSpinner() {
+        // Create adapter for spinner. The list options are from the String array it will use
+        // the spinner will use the default layout
+        ArrayAdapter genderSpinnerAdapter = ArrayAdapter.createFromResource(this,
+                R.array.array_mode_options, android.R.layout.simple_spinner_item);
+
+        // Specify dropdown layout style - simple list view with 1 item per line
+        genderSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+
+        // Apply the adapter to the spinner
+        modeSpinner.setAdapter(genderSpinnerAdapter);
+
+        // Set the integer mSelected to the constant values
+        modeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selection = (String) parent.getItemAtPosition(position);
+                if (!TextUtils.isEmpty(selection)) {
+                    if (selection.equals(getString(R.string.mode_cab))) {
+                        mode = 1;//Cab
+                    } else if (selection.equals(getString(R.string.mode_bus))) {
+                        mode = 2;//Bus
+                    } else if (selection.equals(getString(R.string.mode_train))) {
+                        mode = 3;//Train
+                    } else {
+                        mode = 4;//Air Plane
+                    }
+                }
+            }
+
+            // Because AdapterView is an abstract class, onNothingSelected must be defined
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                mode = 4;
+            }
+        });
     }
 }
--- app/src/main/java/com/example/android/strs/CustomerRegisterActivity.java
+++ app/src/main/java/com/example/android/strs/CustomerRegisterActivity.java
@@ -12,18 +12,68 @@ import android.widget.Spinner;
 
 public class CustomerRegisterActivity extends AppCompatActivity {
 
+        private EditText etAge;
+        private EditText etName;
+        private EditText etUsername;
+        private EditText etPassword;
+        private Button bRegister;
+        private Spinner mGenderSpinner;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customer_register);
+        private int mGender = 0;
 
-        final EditText etAge = (EditText) findViewById(R.id.etAge);
-        final EditText etName = (EditText) findViewById(R.id.etName);
-        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
-        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
-        final Button bRegister = (Button) findViewById(R.id.bRegister);
-        final Spinner mGenderSpinner = (Spinner) findViewById(R.id.spinner_gender);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_customer_register);
 
-    }
+            // Find all relevant views that we will need to read user input from
+            etAge = (EditText) findViewById(R.id.etAge);
+            etName = (EditText) findViewById(R.id.etName);
+            etUsername = (EditText) findViewById(R.id.etUsername);
+            etPassword = (EditText) findViewById(R.id.etPassword);
+            bRegister = (Button) findViewById(R.id.bRegister);
+            mGenderSpinner = (Spinner) findViewById(R.id.spinner_gender);
+
+            setupSpinner();
+
+        }
+
+        /**
+         * Setup the dropdown spinner that allows the user to select the gender of the pet.
+         */
+        private void setupSpinner() {
+            // Create adapter for spinner. The list options are from the String array it will use
+            // the spinner will use the default layout
+            ArrayAdapter genderSpinnerAdapter = ArrayAdapter.createFromResource(this,
+                    R.array.array_gender_options, android.R.layout.simple_spinner_item);
+
+            // Specify dropdown layout style - simple list view with 1 item per line
+            genderSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+            // Apply the adapter to the spinner
+            mGenderSpinner.setAdapter(genderSpinnerAdapter);
+
+            // Set the integer mSelected to the constant values
+            mGenderSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    String selection = (String) parent.getItemAtPosition(position);
+                    if (!TextUtils.isEmpty(selection)) {
+                        if (selection.equals(getString(R.string.gender_male))) {
+                            mGender = 1;//Male
+                        } else if (selection.equals(getString(R.string.gender_female))) {
+                            mGender = 2;//Female
+                        } else {
+                            mGender = 3;//Unknown
+                        }
+                    }
+                }
+
+                // Because AdapterView is an abstract class, onNothingSelected must be defined
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+                    mGender = 3;
+                }
+            });
+        }
 }
